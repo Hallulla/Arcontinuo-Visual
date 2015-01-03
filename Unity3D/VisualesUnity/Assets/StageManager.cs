bf2d97bb9b75cc5820cd8d6990cd4e375f7e353e
Julio Quiroz <chulini@gmail.com>
2015-01-03 17:11:17 -0300

    fade borders

--- Unity3D/VisualesUnity/Assets/StageManager.cs
+++ Unity3D/VisualesUnity/Assets/StageManager.cs
@@ -12,5 +12,16 @@ public class StageManager : MonoBehaviour {
 	void Awake(){
 		instance = this;
 	}
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Alpha0)){
+			StageManager.currentStage = 0;
+		} else if(Input.GetKeyDown(KeyCode.Alpha1)){
+			StageManager.currentStage = 1;
+		} else if(Input.GetKeyDown(KeyCode.Alpha2)){
+			StageManager.currentStage = 2;
+		} else if(Input.GetKeyDown(KeyCode.Alpha3)){
+			StageManager.currentStage = 3;
+		}
+	}
 
 }
